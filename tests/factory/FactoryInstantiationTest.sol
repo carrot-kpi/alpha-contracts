c27cd3d577acc5a5c3dd214df72bdc6ae7252164
Federico Luzzi <fedeluzzi00@gmail.com>
2022-04-20 16:32:19 -0400

    test: implement instantiation tests on factory contract

---
+++ tests/factory/FactoryInstantiationTest.sol
@@ -0,0 +1,37 @@
+pragma solidity 0.8.13;
+
+import {BaseTestSetup} from "../commons/BaseTestSetup.sol";
+import {KPITokensFactory} from "../../contracts/KPITokensFactory.sol";
+
+/**
+ * @title FactoryInstantiationTest
+ * @dev FactoryInstantiationTest contract
+ * @author Federico Luzzi - <fedeluzzi00@gmail.com>
+ * SPDX-License-Identifier: GPL-3.0
+ */
+contract FactoryInstantiationTest is BaseTestSetup {
+    function testZeroAddressKpiTokensManager() external {
+        CHEAT_CODES.expectRevert(
+            abi.encodeWithSignature("ZeroAddressKpiTokensManager()")
+        );
+        new KPITokensFactory(address(0), address(1), address(2));
+    }
+
+    function testZeroAddressOraclesManager() external {
+        CHEAT_CODES.expectRevert(
+            abi.encodeWithSignature("ZeroAddressOraclesManager()")
+        );
+        new KPITokensFactory(address(1), address(0), address(2));
+    }
+
+    function testZeroAddressFeeReceiver() external {
+        CHEAT_CODES.expectRevert(
+            abi.encodeWithSignature("ZeroAddressFeeReceiver()")
+        );
+        new KPITokensFactory(address(1), address(2), address(0));
+    }
+
+    function testSuccess() external {
+        new KPITokensFactory(address(1), address(2), address(3));
+    }
+}
