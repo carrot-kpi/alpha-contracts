9139bdf7c6bbd5ba5bef64ca3b403cc6f8132780
Federico Luzzi <fedeluzzi00@gmail.com>
2022-05-17 11:40:08 +0200

    chore: add kpi tokens factory natspec docs

--- contracts/KPITokensFactory.sol
+++ contracts/KPITokensFactory.sol
@@ -9,12 +9,15 @@ import "./interfaces/IKPITokensManager.sol";
 import "./interfaces/IOraclesManager.sol";
 import "./interfaces/kpi-tokens/IKPIToken.sol";
 
-/**
- * @title KPITokensFactory
- * @dev KPITokensFactory contract
- * @author Federico Luzzi - <federico.luzzi@protonmail.com>
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
+/// SPDX-License-Identifier: GPL-3.0-or-later
+/// @title KPI tokens factory
+/// @dev The factory contract acts as an entry point for users wanting to
+/// create a KPI token., passing as input the id of the template that is
+/// to be used, alongside the description's IPFS cid (pointing to a
+/// structured JSON describing what the KPI token is about) and the oracles
+/// initialization data (template-specific). Other utility view functions
+/// are included to query the storage of the contract.
+/// @author Federico Luzzi - <federico.luzzi@protonmail.com>
 contract KPITokensFactory is Ownable, IKPITokensFactory {
     using SafeERC20 for IERC20;
 
@@ -45,6 +48,12 @@ contract KPITokensFactory is Ownable, IKPITokensFactory {
         feeReceiver = _feeReceiver;
     }
 
+    /// @dev Creates a KPI token with the input data.
+    /// @param _id The id of the KPI token template to be used.
+    /// @param _description An IPFS cid pointing to a structured JSON describing what the KPI token is about.
+    /// @param _initializationData The template-specific ABI-encoded initialization data.
+    /// @param _oraclesInitializationData The initialization data required by the template to initialize
+    /// the linked oracles.
     function createToken(
         uint256 _id,
         string calldata _description,
@@ -74,6 +83,8 @@ contract KPITokensFactory is Ownable, IKPITokensFactory {
         kpiTokens.push(_instance);
     }
 
+    /// @dev KPI tokens manager address setter. Can only be called by the contract owner.
+    /// @param _kpiTokensManager The new KPI tokens manager address.
     function setKpiTokensManager(address _kpiTokensManager) external {
         if (msg.sender != owner()) revert Forbidden();
         if (_kpiTokensManager == address(0))
@@ -81,22 +92,32 @@ contract KPITokensFactory is Ownable, IKPITokensFactory {
         kpiTokensManager = _kpiTokensManager;
     }
 
+    /// @dev Oracles manager address setter. Can only be called by the contract owner.
+    /// @param _oraclesManager The new oracles manager address.
     function setOraclesManager(address _oraclesManager) external {
         if (msg.sender != owner()) revert Forbidden();
         if (_oraclesManager == address(0)) revert ZeroAddressOraclesManager();
         oraclesManager = _oraclesManager;
     }
 
+    /// @dev Fee receiver address setter. Can only be called by the contract owner.
+    /// @param _feeReceiver The new fee receiver address.
     function setFeeReceiver(address _feeReceiver) external {
         if (msg.sender != owner()) revert Forbidden();
         if (_feeReceiver == address(0)) revert ZeroAddressFeeReceiver();
         feeReceiver = _feeReceiver;
     }
 
+    /// @dev Gets the amount of all created KPI tokens.
+    /// @return The KPI tokens amount.
     function kpiTokensAmount() external view override returns (uint256) {
         return kpiTokens.length;
     }
 
+    /// @dev Gets a KPI tokens slice based on indexes.
+    /// @param _fromIndex The index from which to get KPI tokens.
+    /// @param _toIndex The maximum index to which to get KPI tokens.
+    /// @return An address array representing the slice taken between the given indexes.
     function enumerate(uint256 _fromIndex, uint256 _toIndex)
         external
         view
